e611c40b4a42a018f17ea40812ad9826e5c98724
Olle Olsson <ohjolsson@gmail.com>
2017-04-20 00:12:16 +0200

    Removed longclick to direct bombardment. Clicking within range outside army body no longer caught

--- Assets/scripts/Artillery.cs
+++ Assets/scripts/Artillery.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
-using System;
 
 public class Artillery : Army
 {
@@ -14,20 +13,14 @@ public class Artillery : Army
     [SerializeField]
     private float bombardRange;
 
-    private const float MIN_SWIPE_TIME = 0.5f;
     private const float SHELL_LIFETIME = 0.7f;
 
-    private Vector2 invalidPos = new Vector2(-1, -1);
-    private Vector2 bombardTarget;
-    private float swipeStartTime = -1;
     private float remainingReloadTime;
-    private bool acceptNewPath;
     private bool inBombardMode;
 
     protected override void Awake()
     {
         base.Awake();
-        bombardTarget = invalidPos;
     }
 
     void Update()
@@ -36,16 +29,11 @@ public class Artillery : Army
         {
             remainingReloadTime -= Time.deltaTime;
         }
-        else if (IsBombaring())
+        else if (inBombardMode)
         {
-            Bombard();
+            //Bombard();
             remainingReloadTime = reloadTime;
         }
-    }
-
-    private bool IsBombaring()
-    {
-        return inBombardMode && bombardTarget != invalidPos;
     }
 
     private bool IsReloading()
@@ -53,39 +41,34 @@ public class Artillery : Army
         return remainingReloadTime > 0;
     }
 
-    private bool DidLongGesture()
-    {
-        return swipeStartTime >= 0 && swipeStartTime + MIN_SWIPE_TIME <= Time.time;
-    }
+    //private void Bombard()
+    //{
+    //    var shell = Instantiate(shellPrefab);
+    //    var sr = shell.GetComponent<SpriteRenderer>();
+    //    shell.transform.position = bombardTarget;
+    //    var color = sr.color;
+    //    color.a = 0.5f;
+    //    sr.color = color;
+    //    Destroy(shell, SHELL_LIFETIME);
 
-    private void Bombard()
-    {
-        var shell = Instantiate(shellPrefab);
-        var sr = shell.GetComponent<SpriteRenderer>();
-        shell.transform.position = bombardTarget;
-        var color = sr.color;
-        color.a = 0.5f;
-        sr.color = color;
-        Destroy(shell, SHELL_LIFETIME);
-
-        var colls = Physics2D.OverlapCircleAll(bombardTarget, shellDamageRadius);
-        foreach (var coll in colls)
-        {
-            var army = coll.gameObject.GetComponent<Army>();
-            if (army != null && IsEnemy(army))
-            {
-                var rb = army.GetComponent<Rigidbody2D>();
-                var forceDirection = (Vector2)coll.transform.position - bombardTarget;
-                rb.AddForce(forceDirection * 100);
-                army.TakeDamage(bombardDamge);
-            }
-        }
-    }
+    //    var colls = Physics2D.OverlapCircleAll(bombardTarget, shellDamageRadius);
+    //    foreach (var coll in colls)
+    //    {
+    //        var army = coll.gameObject.GetComponent<Army>();
+    //        if (army != null && IsEnemy(army))
+    //        {
+    //            var rb = army.GetComponent<Rigidbody2D>();
+    //            var forceDirection = (Vector2)coll.transform.position - bombardTarget;
+    //            rb.AddForce(forceDirection * 100);
+    //            army.TakeDamage(bombardDamge);
+    //        }
+    //    }
+    //}
 
     protected override void Attack(Army enemy)
     {
         // While bombarding, targets in range but not in bombardment area are ignored.
-        if (!inBombardMode)
+        //if (!inBombardMode)
         {
             base.Attack(enemy);
         }
@@ -101,41 +84,9 @@ public class Artillery : Army
             OnRangeChanged(range);
     }
 
-    protected override void OnMouseDown()
-    {
-        base.OnMouseDown();
-        acceptNewPath = false;
-        swipeStartTime = Time.time;
-    }
-
-    protected void OnMouseUp()
-    {
-        if (inBombardMode)
-        {
-            bombardTarget = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            swipeStartTime = -1;
-            return;
-        }
-        if (DidLongGesture())
-        {
-            if (IsStationary())
-                SetBombardMode(true);
-        }
-        swipeStartTime = -1;
-    }
-    
-    private void OnMouseExit()
-    {
-        acceptNewPath = true;
-    }
-
     public override void ChangeTravelPath(List<Vector2> swipePath)
     {
-        if (acceptNewPath)
-        {
-            base.ChangeTravelPath(swipePath);
-            SetBombardMode(false);
-            bombardTarget = invalidPos;
-        }
+        SetBombardMode(false);
+        base.ChangeTravelPath(swipePath);
     }
 }
